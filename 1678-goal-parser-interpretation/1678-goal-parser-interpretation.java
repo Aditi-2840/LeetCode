fedd0283a348317deac040982d80260cac8fcfc4
Aditi Kumari <55599804+Aditi-2840@users.noreply.github.com>
2022-10-11 23:04:15 +0530

    Time: 1 ms (93.06%), Space: 41.6 MB (78.82%) - LeetHub

--- 1678-goal-parser-interpretation/1678-goal-parser-interpretation.java
+++ 1678-goal-parser-interpretation/1678-goal-parser-interpretation.java
@@ -1,12 +1,17 @@
 class Solution {
     public String interpret(String str) {
+        StringBuilder sb=new StringBuilder();
         
-        
-        str=str.replaceAll("\\(\\)","o");
-        str=str.replaceAll("\\(al\\)","al");
-        
-        return str;
-        
-        
+        for(int i=0;i<str.length();i++){
+            if(str.charAt(i)=='G')
+                sb.append('G');
+            else if(str.charAt(i)=='(' && str.charAt(i+1)==')'){
+                sb.append('o');
+            }
+            else if(str.charAt(i)=='(' && str.charAt(i+1)=='a' && str.charAt(i+2)=='l' && str.charAt(i+3)==')'){
+                sb.append("al");
+            }
+        }
+       return sb.toString();
     }
 }
